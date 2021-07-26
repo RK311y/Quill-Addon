69836ae4887a2ac44136346167220711da9a52f7
River Kelly <k.riv208@gmail.com>
2021-07-26 15:54:53 -0600

    Create addon.setup.php

---
+++ addon.setup.php
@@ -0,0 +1,15 @@
+<?php
+
+return array(
+    'name' => 'Quill',
+    'description' => 'Quill-Addon description', /* TODO: add proper addon description */
+    'version' => '1.0.0',
+    'author' => 'High Altitude Technologies',
+    'author_url' => 'https://high-altitude-tech.com',
+    'namespace' => 'User\Addons\Quill',
+    'settings_exist' => true
+);
+// END OF
+
+/* End of file addon.setup.php */
+/* Location: ./system/user/addons/quill/addon.setup.php */
