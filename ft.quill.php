a9901209cea5b8a31c7f43ddd522d06820fadb43
River Kelly <k.riv208@gmail.com>
2021-07-26 15:58:58 -0600

    Create ft.quill.php

---
+++ ft.quill.php
@@ -0,0 +1,28 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * Quill Field Class
+ * 
+ */
+class Quill_ft extends EE_Fieldtype
+{
+
+    var $info = array(
+        'name'      => 'Quill',
+        'version'   => '1.0.0'
+    );
+
+    public function display_field($data)
+    {
+        return form_input(array(
+            'name'  => $this->field_name,
+            'id'    => $this->field_id,
+            'value' => $data
+        ));
+    }
+
+}
+// END Quill_ft class
+
+/* End of file ft.quill.php */
+/* Location: ./system/user/addons/quill/ft.quill.php */
